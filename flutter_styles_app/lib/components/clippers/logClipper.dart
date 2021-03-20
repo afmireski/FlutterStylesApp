bc245c42b8ee6f013c62ea84bc183422f89e880a
afmireski <afmireski@outlook.com>
2021-03-20 16:25:23 -0300

    Login.dart responsiva para WEB

--- flutter_styles_app/lib/components/clippers/logClipper.dart
+++ flutter_styles_app/lib/components/clippers/logClipper.dart
@@ -1,33 +1,69 @@
 import 'package:flutter/material.dart';
 
 class LogClipper extends CustomClipper<Path> {
+
+  final BoxConstraints constraints;
+  final double maxHeight;
+
+  LogClipper({@required this.constraints, @required this.maxHeight});
+
   @override
   Path getClip(Size size) {
     Path path = new Path();
 
-    ///TOP
-    path.moveTo(size.width * 0.9, 0);
-    path.cubicTo(
-        size.width * 0.6, size.height * 0.35,
-        size.width * 0.05, 0,
-        0, size.height * 0.2);
-    path.lineTo(0, size.height * 0.2);
+    if (constraints.maxHeight <= 800 || size.height <= 425) {
+      ///TOP
+      path.moveTo(size.width * 0.9, 0);
+      path.cubicTo(
+          size.width * 0.6, size.height * 0.35, ///Control Point 1
+          size.width * 0.05, 0, ///Control Point 2
+          0, size.height * 0.2 ///End Point
+      );
 
-    ///LEFT
-    path.lineTo(0, size.height * 0.9);
+      path.lineTo(0, size.height * 0.2);
 
-    ///BOTTOM
-    path.arcToPoint(Offset(size.width * 0.1, size.height), radius: Radius.circular(size.height * 0.1), clockwise: false);
-    path.cubicTo(
-        size.width * 0.2, size.height * 0.85,
-        size.width * 0.4, size.height,
-        size.width * 0.8, size.height * 0.95);
-    path.lineTo(size.width * 0.8, size.height * 0.95);
-    path.arcToPoint(Offset(size.width, size.height * 0.8), radius: Radius.circular(size.height * 0.2), clockwise: false);
+      ///LEFT
+      path.lineTo(0, size.height * 0.9);
 
-    ///RIGHT
-    path.lineTo(size.width, size.height * 0.2);
-    path.arcToPoint(Offset(size.width * 0.9, 0), radius: Radius.circular(size.height * 0.2), clockwise: false);
+      ///BOTTOM
+      path.arcToPoint(Offset(size.width * 0.1, size.height), radius: Radius.circular(size.height * 0.1), clockwise: false);
+      path.cubicTo(
+          size.width * 0.2, size.height * 0.85,
+          size.width * 0.4, size.height,
+          size.width * 0.8, size.height * 0.95);
+      path.lineTo(size.width * 0.8, size.height * 0.95);
+      path.arcToPoint(Offset(size.width, size.height * 0.8), radius: Radius.circular(size.height * 0.2), clockwise: false);
+
+      ///RIGHT
+      path.lineTo(size.width, size.height * 0.2);
+      path.arcToPoint(Offset(size.width * 0.9, 0), radius: Radius.circular(size.height * 0.2), clockwise: false);
+    } else {
+      ///TOP
+      path.moveTo(size.width * 0.9, 0);
+      path.cubicTo(
+          size.width * 0.6, maxHeight * 0.35, ///Control Point 1
+          size.width * 0.05, 0, ///Control Point 2
+          0, maxHeight * 0.2 ///End Point
+      );
+
+      path.lineTo(0, maxHeight * 0.2);
+
+      ///LEFT
+      path.lineTo(0, maxHeight * 0.8);
+
+      ///BOTTOM
+      path.arcToPoint(Offset(size.width * 0.1, maxHeight), radius: Radius.circular(maxHeight * 0.1), clockwise: false);
+      path.cubicTo(
+          size.width * 0.2, maxHeight * 0.85,
+          size.width * 0.4, maxHeight,
+          size.width * 0.8, maxHeight * 0.95);
+      path.lineTo(size.width * 0.8, maxHeight * 0.95);
+      path.arcToPoint(Offset(size.width, maxHeight * 0.8), radius: Radius.circular(maxHeight * 0.3), clockwise: false);
+
+      ///RIGHT
+      path.lineTo(size.width, maxHeight* 0.2);
+      path.arcToPoint(Offset(size.width * 0.9, 0), radius: Radius.circular(maxHeight * 0.2), clockwise: false);
+    }
     path.close();
 
     return path;
--- flutter_styles_app/lib/flutterStylesApp/auth/login.dart
+++ flutter_styles_app/lib/flutterStylesApp/auth/login.dart
@@ -28,161 +28,7 @@ class Login extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20.0,),
-              Container(
-                height: mediaConfig.returnHeight(percent: 0.5644),
-                child: Stack(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 10.0),
-                      child: ClipPath(
-                        clipper: LogClipper(),
-                        child: Container(
-                          padding: EdgeInsets.fromLTRB(10.0, 75.0, 10.0, 50),
-                          color: Colors.white,
-                          height: mediaConfig.returnHeight(percent: 0.5362),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Observer(
-                                builder: (context) {
-                                  return BeveledRectangleTextField(
-                                    label: 'Email',
-                                    onChanged: loginController.loginModel.changeEmail,
-                                    errorText: loginController.validaEmail(),
-                                    errorStyle: TextStyle(color: Color(0xFF5A0FFC), fontWeight: FontWeight.w500),
-                                    fieldStyle: TextStyle(
-                                      color: Theme.of(context).accentColor,
-                                      fontSize: 16,
-                                    ),
-                                    borderColor: Theme.of(context).primaryColorDark,
-                                    shadows: [
-                                      BoxShadow(
-                                          color: Theme.of(context).primaryColorDark,
-                                          offset: Offset(4.0, 2.0),
-                                          blurRadius: 2.0)
-                                    ],
-                                    inputAction: TextInputAction.next,
-                                    keyboardType: TextInputType.emailAddress,
-                                    prefixIcon: Icon(Icons.email_outlined, color: Theme.of(context).primaryColorDark, size: 30.0,),
-                                  );
-                                },
-                              ),
-                              SizedBox(height: 10.0,),
-                              Observer(
-                                builder: (context) {
-                                  return Row(
-                                    children: [
-                                      Expanded(
-                                        child: BeveledRectangleTextField(
-                                          label: 'Senha',
-                                          onChanged: loginController.loginModel.changePassword,
-                                          errorText: loginController.validaPassword(),
-                                          errorStyle: TextStyle(color: Color(0xFF5A0FFC), fontWeight: FontWeight.w500),
-                                          fieldStyle: TextStyle(
-                                            color: Theme.of(context).accentColor,
-                                            fontSize: 16,
-                                          ),
-                                          borderColor: Theme.of(context).primaryColorDark,
-                                          shadows: [
-                                            BoxShadow(
-                                                color: Theme.of(context).primaryColorDark,
-                                                offset: Offset(4.0, 2.0),
-                                                blurRadius: 2.0)
-                                          ],
-                                          inputAction: TextInputAction.done,
-                                          keyboardType: TextInputType.visiblePassword,
-                                          prefixIcon: Icon(Icons.lock_outline_sharp, color: Theme.of(context).primaryColorDark, size: 30.0,),
-                                          isObscure: !loginController.showPassword,
-                                        ),
-                                      ),
-                                      SizedBox(width: 10.0,),
-                                      Container(
-                                        decoration: ShapeDecoration(
-                                            shape: BeveledRectangleBorder(
-                                              side: BorderSide(
-                                                  style: BorderStyle.solid,
-                                                  color: Theme.of(context).primaryColorDark,
-                                                  width: 1.0
-                                              ),
-                                              borderRadius: BorderRadius.circular(10.0),
-                                            ),
-                                            shadows: [
-                                              BoxShadow(
-                                                  color: Theme.of(context).primaryColorDark,
-                                                  offset: Offset(4.0, 2.0),
-                                                  blurRadius: 2.0)
-                                            ],
-                                          color: Colors.white
-                                        ),
-                                        child: IconButton(
-                                          padding: EdgeInsets.all(5.0),
-                                          icon: Icon(!loginController.showPassword ? Icons.visibility_outlined : Icons.visibility_off_outlined,
-                                            color: Theme.of(context).primaryColorDark, size: 30.0,),
-                                          onPressed: () => loginController.changeShowPassword(!loginController.showPassword),
-                                        ),
-                                      )
-                                    ],
-                                  );
-                                }
-                              ),
-                              SizedBox(height: 20.0,),
-                              Observer(
-                                builder: (context) {
-                                  return CheckboxListTile(
-                                    value: loginController.lembrarUsuario,
-                                    onChanged: loginController.changeLembrarUsuario,
-                                    title: Text(
-                                      'Lembrar usuário',
-                                      style: TextStyle(
-                                        color: Theme.of(context).accentColor,
-                                        fontStyle: FontStyle.italic
-                                      ),
-                                    ),
-                                    controlAffinity: ListTileControlAffinity.leading,
-                                  );
-                                }
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      right: mediaConfig.returnWidth(percent: 0.0445),
-                      top: 0,
-                      child: CircleAvatar(
-                        radius: 40.0,
-                        backgroundColor: Theme.of(context).primaryColor,
-                        child: Icon(Icons.person, color: Colors.white,),
-                      ),
-                    ),
-                    Positioned(
-                      left: mediaConfig.returnWidth(percent: 0.0556),
-                        bottom: 0,
-                        child: Observer(
-                          builder: (context) {
-                            return ElevatedButton.icon(
-                              onPressed: loginController.isValid ? () {
-                                Navigator.of(context).pushReplacement(
-                                    MaterialPageRoute(
-                                      builder: (context) => Home(),));
-                              } : null,
-                              icon: Icon(Icons.login, color: Colors.white, size: 30,),
-                              style: ButtonStyle(
-                                shape: MaterialStateProperty.resolveWith((states) => RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)))
-                              ),
-                              label: Text(
-                                'Login',
-                                style: TextStyle(color: Colors.white, fontSize: 18.0),
-                              ),
-
-                            );
-                          }
-                        )
-                    )
-                  ],
-                ),
-              ),
+              _buildLoginForm(mediaConfig),
               SizedBox(height: 30.0,),
               Container(
                 child: Center(
@@ -207,4 +53,186 @@ class Login extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildLoginForm(MediaQueryConfig mediaConfig) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          height: mediaConfig.returnHeight(percent: 0.5644),
+          child: Stack(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                child: Center(
+                  child: ClipPath(
+                    clipper: LogClipper(constraints: constraints, maxHeight: 425),
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(
+                          10.0,
+                          75.0,
+                          10.0,
+                          50
+                      ),
+                      color: Colors.white,
+                      height: mediaConfig.returnHeight(percent: 0.5362),
+                      width: constraints.maxWidth <= 600 ? double.infinity : 540,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Observer(
+                            builder: (context) {
+                              return BeveledRectangleTextField(
+                                label: 'Email',
+                                onChanged: loginController.loginModel.changeEmail,
+                                errorText: loginController.validaEmail(),
+                                errorStyle: TextStyle(color: Color(0xFF5A0FFC), fontWeight: FontWeight.w500),
+                                fieldStyle: TextStyle(
+                                  color: Theme.of(context).accentColor,
+                                  fontSize: 16,
+                                ),
+                                borderColor: Theme.of(context).primaryColorDark,
+                                shadows: [
+                                  BoxShadow(
+                                      color: Theme.of(context).primaryColorDark,
+                                      offset: Offset(4.0, 2.0),
+                                      blurRadius: 2.0)
+                                ],
+                                inputAction: TextInputAction.next,
+                                keyboardType: TextInputType.emailAddress,
+                                prefixIcon: Icon(Icons.email_outlined, color: Theme.of(context).primaryColorDark, size: 30.0,),
+                              );
+                            },
+                          ),
+                          SizedBox(height: 10.0,),
+                          Observer(
+                              builder: (context) {
+                                return Row(
+                                  children: [
+                                    Expanded(
+                                      child: BeveledRectangleTextField(
+                                        label: 'Senha',
+                                        onChanged: loginController.loginModel.changePassword,
+                                        errorText: loginController.validaPassword(),
+                                        errorStyle: TextStyle(color: Color(0xFF5A0FFC), fontWeight: FontWeight.w500),
+                                        fieldStyle: TextStyle(
+                                          color: Theme.of(context).accentColor,
+                                          fontSize: 16,
+                                        ),
+                                        borderColor: Theme.of(context).primaryColorDark,
+                                        shadows: [
+                                          BoxShadow(
+                                              color: Theme.of(context).primaryColorDark,
+                                              offset: Offset(4.0, 2.0),
+                                              blurRadius: 2.0)
+                                        ],
+                                        inputAction: TextInputAction.done,
+                                        keyboardType: TextInputType.visiblePassword,
+                                        prefixIcon: Icon(Icons.lock_outline_sharp, color: Theme.of(context).primaryColorDark, size: 30.0,),
+                                        isObscure: !loginController.showPassword,
+                                      ),
+                                    ),
+                                    SizedBox(width: 10.0,),
+                                    Container(
+                                      decoration: ShapeDecoration(
+                                          shape: BeveledRectangleBorder(
+                                            side: BorderSide(
+                                                style: BorderStyle.solid,
+                                                color: Theme.of(context).primaryColorDark,
+                                                width: 1.0
+                                            ),
+                                            borderRadius: BorderRadius.circular(10.0),
+                                          ),
+                                          shadows: [
+                                            BoxShadow(
+                                                color: Theme.of(context).primaryColorDark,
+                                                offset: Offset(4.0, 2.0),
+                                                blurRadius: 2.0)
+                                          ],
+                                          color: Colors.white
+                                      ),
+                                      child: IconButton(
+                                        padding: EdgeInsets.all(5.0),
+                                        icon: Icon(!loginController.showPassword ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+                                          color: Theme.of(context).primaryColorDark, size: 30.0,),
+                                        onPressed: () => loginController.changeShowPassword(!loginController.showPassword),
+                                      ),
+                                    )
+                                  ],
+                                );
+                              }
+                          ),
+                          SizedBox(height: 20.0,),
+                          Observer(
+                              builder: (context) {
+                                return CheckboxListTile(
+                                  value: loginController.lembrarUsuario,
+                                  onChanged: loginController.changeLembrarUsuario,
+                                  title: Text(
+                                    'Lembrar usuário',
+                                    style: TextStyle(
+                                        color: Theme.of(context).accentColor,
+                                        fontStyle: FontStyle.italic
+                                    ),
+                                  ),
+                                  controlAffinity: ListTileControlAffinity.leading,
+                                );
+                              }
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              ///Avatar
+              Positioned(
+                right: constraints.maxWidth <= 600 ?
+                mediaConfig.returnWidth(percent: 0.0445) :
+                constraints.maxWidth <= 1000 ?
+                mediaConfig.returnWidth(percent: 0.2) :
+                mediaConfig.returnWidth(percent: 0.375),
+                top: 0,
+                child: CircleAvatar(
+                  radius: 40.0,
+                  backgroundColor: Theme.of(context).primaryColor,
+                  child: Icon(Icons.person, color: Colors.white,),
+                ),
+              ),
+              ///Login Button
+              Positioned(
+                  left: constraints.maxWidth <= 600 ?
+                  mediaConfig.returnWidth(percent: 0.0556) :
+                  constraints.maxWidth <= 1000 ?
+                  mediaConfig.returnWidth(percent: 0.2) :
+                  mediaConfig.returnWidth(percent: 0.35),
+                  bottom: constraints.maxWidth <= 600 ? 0 : 110,
+                  child: Observer(
+                      builder: (context) {
+                        return ElevatedButton.icon(
+                          onPressed: loginController.isValid ? () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => Home(),));
+                          } : null,
+                          icon: Icon(Icons.login, color: Colors.white, size: 30,),
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.resolveWith((states) => RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)))
+                          ),
+                          label: Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white, fontSize: 18.0),
+                          ),
+
+                        );
+                      }
+                  )
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
 }
